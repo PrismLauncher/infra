c0dd382724a807f0f782c4a18902601334e435a6
Sefa Eyeoglu <contact@scrumplex.net>
2024-11-25 20:14:23 +0100

    feat(ci): enable linting for GHA
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- treefmt.nix
+++ treefmt.nix
@@ -2,12 +2,14 @@
 {
   projectRootFile = "flake.nix";
 
+  programs.actionlint.enable = true;
   programs.hclfmt.enable = true;
   programs.just.enable = true;
   programs.mdformat.enable = true;
   programs.nixfmt.enable = true;
   programs.shfmt.enable = true;
   programs.terraform.enable = true;
+  programs.yamlfmt.enable = true;
 
   settings.global.excludes = [
     "keys/**.pub"
