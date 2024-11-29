8aa089899435b0f46cdf34d60e78aacb0e2a40d9
Sefa Eyeoglu <contact@scrumplex.net>
2024-11-30 00:19:28 +0100

    tf: add support for ssh_keys
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+locals {
+  user_keys_path = "${path.root}/keys/users"
+  user_keys      = { for f in fileset(local.user_keys_path, "*.pub") : trimsuffix(f, ".pub") => file("${local.user_keys_path}/${f}") }
+}
+
+resource "hcloud_ssh_key" "user_keys" {
+  for_each   = local.user_keys
+  name       = each.key
+  public_key = each.value
+}
+
 resource "hcloud_server" "andesite" {
   name        = "andesite"
   image       = "ubuntu-22.04"
@@ -6,6 +17,12 @@ resource "hcloud_server" "andesite" {
   public_net {
     ipv4_enabled = true
     ipv6_enabled = true
+  }
+
+  ssh_keys = [for k in hcloud_ssh_key.user_keys : k.id]
+
+  lifecycle {
+    ignore_changes = [ssh_keys]
   }
 }
 
