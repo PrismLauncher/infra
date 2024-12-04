911c882aa3bfd9a2ee9a3f5407e4496144dbc204
Sefa Eyeoglu <contact@scrumplex.net>
2024-12-04 17:35:52 +0100

    flake: use nixos-unstable
    Fixes #28
    
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Infrastructure configuration for Prism Launcher";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
