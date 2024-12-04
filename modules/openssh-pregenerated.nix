092e94df4a95ca2cd012713a111ec1656a3e8d66
Sefa Eyeoglu <contact@scrumplex.net>
2024-12-04 22:05:56 +0100

    modules/openssh-pregenerated: use persistent key file path
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- modules/openssh-pregenerated.nix
+++ modules/openssh-pregenerated.nix
@@ -1,5 +1,6 @@
 let
   keyFile = "/etc/ssh/ssh_host_ed25519_key";
+  persistentKeyFile = "/nix/persistence/${keyFile}";
 in
 {
   services.openssh = {
@@ -7,7 +8,8 @@ in
     settings.HostKey = keyFile;
   };
 
+  # age will be activated, before impermanence mounts persistent files and directories. A workaround is to specify the persistent path for the key file
   age.identityPaths = [
-    keyFile
+    persistentKeyFile
   ];
 }
