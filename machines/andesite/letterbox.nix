dfe12887ae92b0015b3d7aad03418c19fa555329
Sefa Eyeoglu <contact@scrumplex.net>
2024-12-30 20:46:04 +0100

    machines/andesite/letterbox: configure settings
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- machines/andesite/letterbox.nix
+++ machines/andesite/letterbox.nix
@@ -9,5 +9,18 @@
   services.letterbox = {
     enable = true;
     environmentFile = config.age.secrets."letterbox.env".path;
+    settings = {
+      server_id = 1031648380885147709;
+      staff_roles = [
+        1031660519335133225
+        1041449261990150255
+      ];
+      forum_channel = {
+        id = 1323366387313279056;
+        open_tag_id = 1323375713402884209;
+        closed_tag_id = 1323375677151383634;
+        mention_role_id = 1061922913839763487;
+      };
+    };
   };
 }
